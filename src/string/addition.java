c85b3ce8abe58d1ea9e04c4ff6d64b70b0f43013
njshakya <njshakya12@hotmail.com>
2024-08-15 09:41:45 -0400

    deletion lines

--- src/string/addition.java
+++ src/string/addition.java
@@ -2,9 +2,9 @@ package string;
 
 public class addition {
 	public static void main(String[] args) {
-		multiply(5,10);
-		addition(7,3);
 	
+		addition(7,3);
+		multiply(5,10);
 		
 	}
 	public static void multiply(int a, int b) {
