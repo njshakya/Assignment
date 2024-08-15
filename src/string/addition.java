a84c59e93934f79d2f34f2949a588e5e670a356a
njshakya <njshakya12@hotmail.com>
2024-08-15 09:39:10 -0400

    second commit

--- src/string/addition.java
+++ src/string/addition.java
@@ -3,12 +3,16 @@ package string;
 public class addition {
 	public static void main(String[] args) {
 		multiply(5,10);
+		addition(7,3);
 	
 		
 	}
 	public static void multiply(int a, int b) {
 		System.out.println(a*b);
 		
+	}
+	public static void addition(int a, int b) {
+		System.out.println(a+b);
 	}
 
 }
